c919349ce1e8d9abadc6214ef244f60133cee3a1
tinosanjaya <80615383+tinosanjaya@users.noreply.github.com>
2021-03-14 13:58:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<title>jQuery Mobile Web App</title>
+<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
+
+<div data-role="page" id="page">
+	<div data-role="header">
+		<h1>Selamat Datang Di</h1>
+        <h1>Pemrograman Mobile</h1>
+	</div>
+	<div data-role="content">	
+		<ul data-role="listview">
+			<li><a href="#page2">Profil</a></li>
+            <li><a href="#page3">Pekerjaan</a></li>
+			<li><a href="#page4">Kontak</a></li>
+		</ul>		
+	</div>
+	<div data-role="footer">
+		<h4>TINO SANJAYA SAPUTRA</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page2">
+	<div data-role="header">
+		<h1>Profil</h1>
+	</div>
+	<div data-role="content">	
+		<h6> Nama	: Tino Sanjaya Saputra</h6>
+        <h6> NPM	: 1810128262165</h6>
+        <h6> Kelas	: 6f</h6>
+        <h6> Umur	: 25</h6>
+        		
+	</div>
+	<div data-role="footer">
+		<h4>TINO SANJAYA SAPUTRA</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page3">
+	<div data-role="header">
+		<h1>Pekerjaan</h1>
+	</div>
+	<div data-role="content">	
+		<h6>Mahasiswa</h6>		
+	</div>
+	<div data-role="footer">
+		<h4>TINO SANJAYA SAPUTRA</h4>
+	</div>
+</div>
+
+<div data-role="page" id="page4">
+	<div data-role="header">
+		<h1>Kontak</h1>
+	</div>
+	<div data-role="content">	
+		<h6>WA	: 087828399494</h6>
+        <h6>IG	: sanjaya.pw</h6>		
+	</div>
+	<div data-role="footer">
+		<h4>TINO SANJAYA SAPUTRA</h4>
+	</div>
+</div>
+
+</body>
+</html>
